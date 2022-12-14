eaa8eed23ec9db76fbfe2150427bb98c9eafbe13
dillakhansa <120298370+dillakhansa@users.noreply.github.com>
2022-12-14 09:16:46 +0700

    Add files via upload

---
+++ Program Booking Tiket Pesawat.py
@@ -0,0 +1,84 @@
+print("|===============================================|")
+print("| LIST KEBERANGKATAN DASPRO LIGHT AIRBUS =======|")
+print("|===============================================|")
+print("| kode penerbangan | tujuan | harga tiket ======|")
+print("|===============================================|")
+print("| 101              | ENG/LDN    | Rp. 6.000.000 |")
+print("| 102              | UKR/KYV    | Rp. 5.000.000 |")
+print("| 103              | AUS/SYD    | Rp. 4.000.000 |")
+print("| 104              | SGN/SING   | Rp. 3.000.000 |")
+print("| 105              | THD/BGK    | Rp. 2.000.000 |")
+print("| 106              | PHP/MNL    | Rp. 1.000.000 |")
+print("|===============================================|")
+print("")
+nama = input("Nama Pembeli : ")
+nomor = input("Nomor Handphone : ")
+asal = input("Asal Kota : ")
+jurusan = input("Kode Tujuan Penerbangan : ")
+tujuan = []
+harga = []
+if jurusan == "101":
+    tujuan.append("LONDON")
+    harga = 6000000
+elif jurusan == "102":
+    tujuan.append("KYIV")
+    harga = 5000000
+elif jurusan == "103":
+    tujuan.append("SYDNEY")
+    harga = 4000000
+elif jurusan == "104":
+    tujuan.append("SINGAPURA")
+    harga = 3000000
+elif jurusan == "105":
+    tujuan.append("BANGKOK")
+    harga = 2000000
+elif jurusan == "106":
+    tujuan.append("MANILA")
+    harga = 1000000
+else :
+    tujuan.append("Kode Yang Anda Masukan Salah")
+
+jumlah = int(input("Jumlah beli pembelian : "))
+seat = input("Nomor seat: ")
+print()
+if jumlah ==2:
+    potongan = int(jumlah*harga)*0.1
+elif jumlah ==3:
+    potongan = int(jumlah*harga)*0.12
+elif jumlah ==4:
+    potongan = int(jumlah*harga)*0.15
+else:
+    potongan =0
+
+total = int(jumlah*harga-potongan)
+pajak = int(total*0.11)
+jumlah_bayar = total+pajak
+def garis():
+
+    print("-------------------------------------")
+garis()
+print("BUKTI PEMESANAN TIKET ANDA")
+garis()
+print("Nama Pembeli : ", nama)
+print("Nomor Handphone : ", nomor)
+print("Asal Negara : ", asal)
+print("Kode Negara : ", jurusan)
+print("Negara Tujuan : ", tujuan)
+print("Jumlah Beli : ", jumlah)
+print("Nomor seat: ", seat)
+
+garis()
+print("Harga tiket : Rp", harga)
+print("Potongan    : Rp", potongan)
+print("PPN 10%     : Rp", pajak)
+garis()
+print("Pelunasan pembayaran tiket")
+print("Jumlah bayar : Rp", jumlah_bayar)
+uang = int(input("Masukan pembayaran : Rp."))
+garis()
+kembalian = uang-jumlah_bayar
+print("Uang kembalian : Rp", kembalian)
+garis()
+print("ENJOY YOUR FLIGHT")
+print("STAY SAFE")
+print("PLEASE FOLLOW COVID-19 PROTOCOL")
